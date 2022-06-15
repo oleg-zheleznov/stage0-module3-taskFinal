65fb0f20cc852de994daf578f10dd34aafabb5a8
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 17:16:06 +0300

    solution

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -4,6 +4,5 @@ public class DivisionByZero {
     public static void main(String[] args) {
         double number = 10;
         System.out.println(number/0);
-        System.out.println(Float.POSITIVE_INFINITY);
     }
 }
